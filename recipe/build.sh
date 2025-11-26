566353dfdb33dc2d8ed484ab775e3e8bab31ffe4
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 19:00:25 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/dlv -ldflags="-s -w" ./cmd/dlv
+go build -o=${PREFIX}/bin/dlv -ldflags="-s -w" ./cmd/dlv
 go-licenses save ./cmd/dlv --save_path=license-files
